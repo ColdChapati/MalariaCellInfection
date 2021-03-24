54ee134cb1afcd301da87656b0aae65c24399a8a
Sidd <62676286+ColdChapati@users.noreply.github.com>
2021-03-24 11:26:36 -0700

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ data = []
 for img in os.listdir('cell_images/Parasitized'):
     try:
         image = cv2.imread(os.path.join('cell_images/Parasitized', img))
-        image = cv2.resize(image, (25, 25))
+        image = cv2.resize(image, (50, 50))
         data.append(image)
         label.append(1)
     except Exception as e:
@@ -28,14 +28,14 @@ for img in os.listdir('cell_images/Parasitized'):
 for img in os.listdir('cell_images/Uninfected'):
     try:
         image = cv2.imread(os.path.join('cell_images/Uninfected', img))
-        image = cv2.resize(image, (25, 25))
+        image = cv2.resize(image, (50, 50))
         data.append(image)
-        label.append(0)
+        label.append(-1)
     except Exception as e:
         pass
 
 # convert and reshape data
-data = np.array(data).reshape(-1,25,25,3)
+data = np.array(data).reshape(-1,50,50,3)
 label = np.array(label)
 
 # split data
@@ -45,17 +45,17 @@ X_train, X_test, y_train, y_test = train_test_split(data, label, train_size=0.9)
 model = Sequential()
 
 # first layer
-model.add(Conv2D(16, kernel_size=(3,3), input_shape=(25,25,3), activation='relu'))
+model.add(Conv2D(32, kernel_size=(3,3), input_shape=(50,50,3), activation='relu'))
 model.add(MaxPooling2D((2,2)))
 
 # second layer
-model.add(Conv2D(32, (3,3), activation='relu'))
+model.add(Conv2D(64, (3,3), activation='relu'))
 model.add(MaxPooling2D((2,2)))
-model.add(Dropout(0.25))
+model.add(Dropout(0.15))
 
 # third layer
 model.add(Flatten())
-model.add(Dense(32, activation='relu'))
+model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.25))
 
 # output layer
@@ -68,10 +68,10 @@ model.summary()
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 # fit model
-results = model.fit(X_train, y_train, batch_size=100, epochs=25, verbose=2, validation_split=0.2)
+results = model.fit(X_train, y_train, batch_size=25, epochs=10, verbose=2, validation_split=0.1)
 
-# fit test data
-model.evaluate(X_test, y_test)
+# save model
+model.save('malaria_weights.h5')
 
 # plot accuracy vs epochs
 plt.plot(results.history['val_accuracy'], label='val_accuracy', color='lightpink')
